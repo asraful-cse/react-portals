806ada4a3c44d59394a1d11c9f9f48e4742de0e1
asraful196 <asraful.01110@gmail.com>
2022-12-15 12:40:34 +0600

    root id added index.htm file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import React, { useState } from 'react'
-import Modal from './Modal'
+import React, { useState } from "react";
+import Modal from "./components/Modal/Modal";
 
-const BUTTON_WRAPPER_STYLES = {
-  position: 'relative',
-  zIndex: 1
-}
-
-const OTHER_CONTENT_STYLES = {
-  position: 'relative',
-  zIndex: 2,
-  backgroundColor: 'red',
-  padding: '10px'
-}
 
 export default function App() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <div style={BUTTON_WRAPPER_STYLES} onClick={() => console.log('clicked')}>
+      <div
+        style={{ position: "relative", zIndex: "1" }}
+        onClick={() => console.log("clicked")}
+      >
         <button onClick={() => setIsOpen(true)}>Open Modal</button>
 
         <Modal open={isOpen} onClose={() => setIsOpen(false)}>
-          Fancy Modal
+          Modal tex ------------------
         </Modal>
       </div>
 
-      <div style={OTHER_CONTENT_STYLES}>Other Content</div>
+      <div
+        style={{
+          position: "relative",
+          zIndex: 2,
+          backgroundColor: "red",
+          padding: "10px",
+        }}
+      >
+        Other Content--
+      </div>
     </>
-  )
+  );
 }
